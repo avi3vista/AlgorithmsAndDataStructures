276379eb61fdb3a0eb59b4c08aedd1caa143f3c8
Avinash Dathathri <adathathri@netflix.com>
2020-09-05 01:41:22 -0400

    Added edit distance using recursion

---
+++ src/main/java/recursion/EditDistance.java
@@ -0,0 +1,56 @@
+package recursion;
+
+import java.util.logging.Logger;
+
+public class EditDistance {
+
+    public static void main(String[] args) {
+        Logger lg = Logger.getLogger("Palindrome");
+        String src = "all";
+        String dst = "ball";
+        int result = editDistanceCount(src.toCharArray(), dst.toCharArray(), 0 ,0);
+        lg.info("Edit distance count for '" + src + "'  to make it to '" + dst+ "' = "+ result);
+
+         src = "cat";
+         dst = "bcats";
+         result = editDistanceCount(src.toCharArray(), dst.toCharArray(), 0 ,0);
+        lg.info("Edit distance count for '" + src + "'  to make it to '" + dst+ "' = "+ result);
+
+
+
+        src = "abcdefghij";
+        dst = "akj";
+        result = editDistanceCount(src.toCharArray(), dst.toCharArray(), 0 ,0);
+        lg.info("Edit distance count for '" + src + "'  to make it to '" + dst+ "' = "+ result);
+
+
+        src = "abc";
+        dst = "xxxabcyyyzzz";
+        result = editDistanceCount(src.toCharArray(), dst.toCharArray(), 0 ,0);
+        lg.info("Edit distance count for '" + src + "'  to make it to '" + dst+ "' = "+ result);
+
+        src = "cat";
+        dst = "cat";
+        result = editDistanceCount(src.toCharArray(), dst.toCharArray(), 0 ,0);
+        lg.info("Edit distance count for '" + src + "'  to make it to '" + dst+ "' = "+ result);
+
+
+
+    }
+
+    private static int editDistanceCount(char[] src, char[] dst, int srcIdx, int dstIdx) {
+        if(srcIdx==src.length) {
+            return dst.length-dstIdx;
+        }
+        if(dstIdx==dst.length) {
+            return src.length-srcIdx;
+        }
+        if(src[srcIdx]==dst[dstIdx]) {
+            return editDistanceCount(src, dst, srcIdx+1, dstIdx+1);
+        }
+        return 1 + Math.min(
+                editDistanceCount(src,dst, srcIdx, dstIdx+1),
+                Math.min(editDistanceCount(src, dst, srcIdx+1, dstIdx),
+                        editDistanceCount(src,dst,srcIdx+1, dstIdx+1)));
+    }
+}
